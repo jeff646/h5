518fdb0b6f7088741f846f3375e20c701fc110e1
jeff646 <mjunhybgt0086@gmail.com>
2024-08-15 19:19:34 +0530

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   mode: 'production',
-  entry: './src/index.js',
+  entry: './src/main.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'public')
